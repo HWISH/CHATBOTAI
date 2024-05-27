de71344337548b9fbae4c8eb73223d4f47d3be86
HWISH <hamzaeloui123@gmail.com>
2024-05-27 21:55:32 +0100

    dsfg

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import { OpenAI } from "openai";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 1520;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
